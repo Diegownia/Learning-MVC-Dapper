7c17a18bf1e3af4394c2e606e7bff4aca6c02f3d
Diegownia <Pkorczak93@gmail.com>
2022-10-21 14:14:22 +0100

    Add project files.

---
+++ MVCCoreApp/Interfaces/Patient.cs
@@ -0,0 +1,21 @@
+﻿using Dapper.Contrib.Extensions;
+using MVCCoreApp.Models;
+
+namespace MVCCoreApp.Interfaces
+{
+    public class Patient : BaseModel, IPatient
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+
+        [Computed]
+        public string FullName
+        {
+            get => Name + Surname;
+        }
+
+        public DateTime? NextVisit { get; set; }
+
+        public DateTime? LastVisit { get; set; }
+    }
+}
---
+++ MVCCoreApp/Models/IPatient.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCCoreApp.Models
+{
+    public interface IPatient
+    {
+        string FullName { get; }
+        DateTime? LastVisit { get; set; }
+        string? Name { get; set; }
+        DateTime? NextVisit { get; set; }
+        string? Surname { get; set; }
+    }
+}
